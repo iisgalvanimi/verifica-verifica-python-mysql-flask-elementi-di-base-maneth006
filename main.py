fc73c558076a435f6571005218a7beddce93c95e
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-11-08 11:37:47 +0000

    commit 3

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ mycursor = mydb.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS verifica")
 mycursor.execute("USE verifica")
 
-
-mycursor.execute("SELECT * FROM musica")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
+rispUtente1 = int(input('Quante album vorresti inserire: '))
+for _ in range(rispUtente1):
+  Artista = input("Nome dell'artista della canzone: ")
+  Album = input("Quale album vorresti inserire: ")
+  Genere = input("Genere dell'artista della canzone: ")
+  Anno_pubblicazione = input("Anno di pubblicazione dell'album: ")
+  Etichetta_discografica = input("Nome dell'etichetta discografica: ")
+  sql = "INSERT INTO musica (Artista, Album, Genere, Anno_pubblicazione, Etichetta_discografica) VALUES (%s, %s, %s, %s, %s)"
+  val = (Artista, Album, Genere, Anno_pubblicazione, Etichetta_discografica)
+  mycursor.execute(sql, val)
+  mydb.commit()
+  print(mycursor.rowcount, "inserito")   
